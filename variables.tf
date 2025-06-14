821c7d6f75239d2a0915fcaef9f05aed8d9cad03
Aaron K <greenlantern73@gmail.com>
2025-06-13 18:20:41 -0700

    Syntax fixes

--- variables.tf
+++ variables.tf
@@ -3,45 +3,40 @@ variable "instance_type" {
   default     = "t3.nano"
 }
 
-
 variable "ami_filter" {
   description = "Name filter and owner for AMI"
 
-  type = object({
-    name  = string
-    owner = string
+  type        = object ({
+    name      = string
+    owner     = string
   })
 
-  default = {
-    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
-    owner = "979382823631"] # Bitnami
+  default     = {
+    name      = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner     = "979382823631" # Bitnami
   }
 }
     
-
 variable "environment" {
   description = "Development Environment"
 
-  type = object({
+  type        = object ({
     name           = string
     network_prefix = string
   })
 
-  default = {
+  default     = {
     name           = "dev"
     network_prefix = "10.0"
   }
 }
-
 
 variable "asg_min_size" {
   description = "Minimum number of instances in the ASG"
   default     = 1
 }
 
-
 variable "asg_max_size" {
   description = "Maximum number of instances in the ASG"
   default     = 2
 }
-
